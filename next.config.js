d3cb9c9026396cd5f732ab4c7b126cc2bad6d531
Yuku TAKAHASHI <taka84u9@gmail.com>
2018-12-25 15:51:37 +0900

    Export custom 404 page for gh-pages

--- next.config.js
+++ next.config.js
@@ -4,6 +4,7 @@ const withTypescript = require("@zeit/next-typescript")
 module.exports = withTypescript(withSass({
   exportPathMap: () => ({
     "/": { page: "/" },
+    "/404.html": { page: "/_error" },
     "/about": { page: "/about" },
     "/resume": { page: "/resume" },
   }),
---
+++ pages/_error.tsx
@@ -0,0 +1,11 @@
+import Header from "../src/components/Header/Header"
+
+export default () => (
+  <div>
+    <Header className="bg-dark" fullScreen>
+      <div className="text-white">
+        <h1 className="display-1">404 Not Found</h1>
+      </div>
+    </Header>
+  </div>
+)
