621b66976ea3bb350d5cccf9cc699e264f9d63e5
Yuku TAKAHASHI <taka84u9@gmail.com>
2018-12-25 17:40:42 +0900

    Add icons

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -25,7 +25,26 @@ class MyApp extends App {
         <Head>
           <meta charSet="utf-8" />
           <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-          <title key="title">Yuku Takahashi</title>
+          <title key="title">SELECT * FROM life;</title>
+          <link rel="apple-touch-icon" sizes="57x57" href="/static/icons/apple-icon-57x57.png" />
+          <link rel="apple-touch-icon" sizes="60x60" href="/static/icons/apple-icon-60x60.png" />
+          <link rel="apple-touch-icon" sizes="72x72" href="/static/icons/apple-icon-72x72.png" />
+          <link rel="apple-touch-icon" sizes="76x76" href="/static/icons/apple-icon-76x76.png" />
+          <link rel="apple-touch-icon" sizes="114x114" href="/static/icons/apple-icon-114x114.png" />
+          <link rel="apple-touch-icon" sizes="120x120" href="/static/icons/apple-icon-120x120.png" />
+          <link rel="apple-touch-icon" sizes="144x144" href="/static/icons/apple-icon-144x144.png" />
+          <link rel="apple-touch-icon" sizes="152x152" href="/static/icons/apple-icon-152x152.png" />
+          <link rel="apple-touch-icon" sizes="180x180" href="/static/icons/apple-icon-180x180.png" />
+          <link rel="icon" type="image/png" sizes="192x192" href="/static/icons/android-icon-192x192.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/static/icons/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="96x96" href="/static/icons/favicon-96x96.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/static/icons/favicon-16x16.png" />
+          <link rel="icon" href="/static/icons/favicon.ico" />
+          <link rel="manifest" href="/static/manifest.json" />
+          <meta name="msapplication-TileColor" content="#00BCD4" />
+          <meta name="msapplication-TileImage" content="/static/icons/ms-icon-144x144.png" />
+          <meta name="msapplication-config" content="/static/browserconfig.xml" />
+          <meta name="theme-color" content="#00BCD4" />
           <link
             href="https://fonts.googleapis.com/css?family=Noto+Sans+JP:300,400,900&amp;subset=japanese"
             rel="stylesheet"
