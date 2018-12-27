6d8ee2a8d8596c74f972564f48f08a44caa89c47
Yuku TAKAHASHI <taka84u9@gmail.com>
2018-12-27 22:16:01 +0900

    Setup @zeit/next-mdx plugin

--- next.config.js
+++ next.config.js
@@ -1,11 +1,14 @@
 const withSass = require("@zeit/next-sass")
 const withTypescript = require("@zeit/next-typescript")
+const withMDX = require("@zeit/next-mdx")()
 
 module.exports = withTypescript(
-  withSass({
-    exportPathMap: defaultMap => ({
-      ...defaultMap,
-      "/404.html": { page: "/_error" },
+  withSass(
+    withMDX({
+      exportPathMap: defaultMap => ({
+        ...defaultMap,
+        "/404.html": { page: "/_error" },
+      }),
     }),
-  }),
+  ),
 )
