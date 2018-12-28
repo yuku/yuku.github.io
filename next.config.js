51e49d93112358688a1229c5dd0bcd4f6d73a51d
Yuku TAKAHASHI <taka84u9@gmail.com>
2018-12-28 20:49:48 +0900

    Use highlight.js with hybrid style

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 const withSass = require("@zeit/next-sass")
 const withTypescript = require("@zeit/next-typescript")
-const withMDX = require("@zeit/next-mdx")()
+
+const highlight = require("rehype-highlight")
+const withMDX = require("@zeit/next-mdx")({
+  options: {
+    hastPlugins: [highlight],
+  },
+})
 
 module.exports = withTypescript(
   withSass(
--- src/components/BlogPage/BlogPage.tsx
+++ src/components/BlogPage/BlogPage.tsx
@@ -1,3 +1,5 @@
+import Head from "next/head"
+
 import { IMeta } from "*.mdx"
 import SingleCol from "../SingleCol/SingleCol"
 import Header from "../Header/Header"
@@ -8,6 +10,9 @@ interface IProps extends IMeta {
 
 const BlogPage = (props: IProps) => (
   <div>
+    <Head>
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.13.1/styles/hybrid.min.css" />
+    </Head>
     <Header className="mb-5" backgroundImage={props.backgroundImage} style={{ backgroundColor: props.backgroundColor }}>
       <SingleCol className="text-center text-white">
         <h1 className="display-4 font-weight-bold">{props.title}</h1>
