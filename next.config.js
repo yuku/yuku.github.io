399d3307b6e119bf1470e826ba23ed824f2b5463
Yuku TAKAHASHI <taka84u9@gmail.com>
2019-01-08 13:08:10 +0900

    Use remark-github

--- next.config.js
+++ next.config.js
@@ -1,12 +1,13 @@
 const withSass = require("@zeit/next-sass")
 const withTypescript = require("@zeit/next-typescript")
 
+const github = require("remark-github")
 const math = require("remark-math")
 const prism = require("rehype-prism")
 const katex = require("rehype-katex")
 const withMDX = require("@zeit/next-mdx")({
   options: {
-    mdPlugins: [math],
+    mdPlugins: [math, [github, { repository: "dummy/repo", mentionStrong: false }]],
     hastPlugins: [prism, katex],
   },
 })
