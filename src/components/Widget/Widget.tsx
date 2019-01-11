b883402bec624349235a30d63ef6daefe814abca
Yuku TAKAHASHI <taka84u9@gmail.com>
2019-01-11 14:21:43 +0900

    Make outline clean

--- src/components/Widget/Widget.tsx
+++ src/components/Widget/Widget.tsx
@@ -10,7 +10,7 @@ interface IProps {
 
 export default (props: IProps) => (
   <aside className={cn("widget mb-4", props.className)}>
-    <div className="title font-weight-bold">{props.title}</div>
+    <h1 className="title font-weight-bold">{props.title}</h1>
     {props.children}
   </aside>
 )
