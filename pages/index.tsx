da6bb884ebe0858db8108ab9287482524c63cd0a
Yuku TAKAHASHI <taka84u9@gmail.com>
2019-01-13 15:30:29 +0900

    Use getInitialProps to load entries

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,37 +1,51 @@
+import React from "react"
 import Link from "next/link"
 import moment from "moment"
 
-import entries from "../src/entries"
+import { IMeta } from "*.mdx"
 
-export default () => (
-  <div className="container">
-    <div className="row">
-      <div className="col-12">
-        {entries.map(entry => (
-          <article key={entry.href} className="mb-4">
-            <Link href={entry.href}>
-              <a href={entry.href} className="text-decoration-none">
-                <h1>{entry.title}</h1>
-              </a>
-            </Link>
-            <ul className="list-inline text-dark font-weight-light">
-              <li className="list-inline-item">
-                <i className="fas fa-calendar-day fa-fw" />
-                <span className="ml-1">{moment(entry.publishedAt).format("YYYY-MM-DD HH:mm")}</span>
-              </li>
-              <li className="list-inline-item">
-                <i className="fas fa-tags fa-fw" />
-                {entry.tags.map((tag, i) => [
-                  i > 0 ? "," : null,
-                  <span key={i} className="ml-1">
-                    {tag}
-                  </span>,
-                ])}
-              </li>
-            </ul>
-          </article>
-        ))}
+interface IProps {
+  entries: (IMeta & { href: string })[]
+}
+
+export default class extends React.Component<IProps> {
+  static async getInitialProps() {
+    const entries = require("../src/entries").default
+    return { entries }
+  }
+
+  public render() {
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-12">
+            {this.props.entries.map(entry => (
+              <article key={entry.href} className="mb-4">
+                <Link href={entry.href}>
+                  <a href={entry.href} className="text-decoration-none">
+                    <h1>{entry.title}</h1>
+                  </a>
+                </Link>
+                <ul className="list-inline text-dark font-weight-light">
+                  <li className="list-inline-item">
+                    <i className="fas fa-calendar-day fa-fw" />
+                    <span className="ml-1">{moment(entry.publishedAt).format("YYYY-MM-DD HH:mm")}</span>
+                  </li>
+                  <li className="list-inline-item">
+                    <i className="fas fa-tags fa-fw" />
+                    {entry.tags.map((tag, i) => [
+                      i > 0 ? "," : null,
+                      <span key={i} className="ml-1">
+                        {tag}
+                      </span>,
+                    ])}
+                  </li>
+                </ul>
+              </article>
+            ))}
+          </div>
+        </div>
       </div>
-    </div>
-  </div>
-)
+    )
+  }
+}
