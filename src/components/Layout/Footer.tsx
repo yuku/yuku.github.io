6a916bebe9e445df7a38f24a55b81306ddd7af86
Yuku TAKAHASHI <taka84u9@gmail.com>
2019-01-16 14:12:51 +0900

    License under a CC BY-NC-SA 4.0

--- src/components/Layout/Footer.tsx
+++ src/components/Layout/Footer.tsx
@@ -2,6 +2,11 @@ const currentYear = new Date().getFullYear()
 
 export default () => (
   <footer className="text-center mt-4">
-    <p className="mb-0 text-dark">Copyright &copy; Yuku Takahashi {currentYear}</p>
+    <p className="mb-0 text-dark">
+      <small>
+        &copy; Yuku Takahashi {currentYear} - This work is licensed under a{" "}
+        <a href="https://creativecommons.org/licenses/by-nc-sa/4.0/">CC BY-NC-SA 4.0 </a>
+      </small>
+    </p>
   </footer>
 )
