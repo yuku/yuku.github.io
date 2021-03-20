69026897fe2dc7fd5fb393d3cafc9f0a4679bdfa
Yuku Takahashi <taka84u9@gmail.com>
2021-03-20 15:19:32 +0900

    Fix warning

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,16 +1,16 @@
 import React from "react"
-import Document, { Head, Main, NextScript } from "next/document"
+import Document, { Html, Head, Main, NextScript } from "next/document"
 
 class MyDocument extends Document {
   public render(): JSX.Element {
     return (
-      <html prefix="og: http://ogp.me/ns#">
+      <Html prefix="og: http://ogp.me/ns#">
         <Head />
         <body>
           <Main />
           <NextScript />
         </body>
-      </html>
+      </Html>
     )
   }
 }
